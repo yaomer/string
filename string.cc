709775c34ed48d287dcb5bb95f51adc65e5bda5a
妖冶如你 <yaoyeruni@gmail.com>
2019-03-27 23:39:27 +0800

    添加test.cc

--- string.cc
+++ string.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "string.h"
 
-using namespace std;
-
 void
 String::__init()
 {
---
+++ test.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+#include "string.h"
+
+using namespace std;
+
+int
+main(void)
+{
+
+}
